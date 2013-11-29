d800facbfed7593ec3645f26b6b3135366079125
Preston Rohner <prestonrohner@me.com>
2013-11-29 09:34:06 -0800

    Add recent file list on main screen

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,8 @@
 class WelcomeController < ApplicationController
   def index
-    @apps = App.all
+    @apps = App.all.order("title")
     @filecount = UploadFile.all.count
+    @upload_files = UploadFile.all.order("filename desc")
+    
   end
 end
